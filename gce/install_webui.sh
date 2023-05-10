554208bab7befdc05a3f57e381e598f9ce436f16
Susumu OTA <1632335+susumuota@users.noreply.github.com>
2023-05-11 02:29:12 +0900

    feat: add install_webui.sh.

---
+++ gce/install_webui.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+sudo apt-get update && sudo apt-get install -y --no-install-recommends \
+  aria2 \
+  emacs \
+  git \
+  libgl1 \
+  libglib2.0-0 \
+  python-is-python3 \
+  python3 \
+  python3-pip \
+  screen \
+  && rm -rf /var/lib/apt/lists/*
+
+
+git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui.git
+cd stable-diffusion-webui
+
+python -u launch.py --skip-torch-cuda-test --xformers --exit
+
+(cd ~/.local/lib/python3.10/dist-packages/torch/lib && ln -s libnvrtc-672ee683.so.11.2 libnvrtc.so)
+
+echo "run 'python -u launch.py --xformers --no-half-vae'"
