78041c13dce05aa0fbd88af606d4362beb47b55d
Susumu OTA <1632335+susumuota@users.noreply.github.com>
2023-05-11 06:04:47 +0900

    fix: typo.

--- gce/rsync_local.sh
+++ gce/rsync_local.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-src="gs://sd-outputs-2/outputs"
+src="gs://sd-outputs-1/outputs"
 dst="outputs"
-sleep_seconds=60
+wait=60
 
 # mkdir -p outputs
-while true ; do gsutil -m rsync -r "$src" "$dst" ; sleep $sleep_seconds ; done
+while true ; do gsutil -m rsync -r "$src" "$dst" ; sleep $wait ; done
--- gce/rsync_remote.sh
+++ gce/rsync_remote.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 src="outputs"
-dst="gs://sd-outputs-2/outputs"
-sleep_seconds=60
+dst="gs://sd-outputs-1/outputs"
+wait=60
 
 # cd stable-diffusion-webui
-while true ; do gsutil -m rsync -r "$src" "$dst" ; sleep $sleep_seconds ; done
+while true ; do gsutil -m rsync -r "$src" "$dst" ; sleep $wait ; done
