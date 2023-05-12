af6f911cc7259d2d29e4690895953202add3d2ed
Susumu OTA <1632335+susumuota@users.noreply.github.com>
2023-05-12 23:20:56 +0900

    fix: merge config.json.

--- gce/install_webui.sh
+++ gce/install_webui.sh
@@ -19,4 +19,15 @@ python -u launch.py --skip-torch-cuda-test --xformers --exit
 
 (cd ~/.local/lib/python3.10/site-packages/torch/lib && ln -s libnvrtc-672ee683.so.11.2 libnvrtc.so)
 
+cat >> config.json <<EOF
+{
+    "grid_save": false,
+    "grid_save_to_dirs": false,
+    "return_grid": false,
+    "js_modal_lightbox_initially_zoomed": false,
+    "live_previews_enable": false,
+    "show_progress_grid": false
+}
+EOF
+
 echo "run 'python -u launch.py --xformers --no-half-vae'"
--- gce/install_webui_cpu.sh
+++ gce/install_webui_cpu.sh
@@ -17,6 +17,18 @@ cd stable-diffusion-webui
 
 sed -i -e "/pip install torch/ s/cu[0-9]\{3,\}/cpu/g" launch.py
 sed -i -e 's/    return "cpu"/    torch.set_num_threads(4)\n    return "cpu"/' modules/devices.py
+
 python -u launch.py --skip-torch-cuda-test --exit
+
+cat >> config.json <<EOF
+{
+    "grid_save": false,
+    "grid_save_to_dirs": false,
+    "return_grid": false,
+    "js_modal_lightbox_initially_zoomed": false,
+    "live_previews_enable": false,
+    "show_progress_grid": false
+}
+EOF
 
 echo "run 'python -u launch.py --skip-torch-cuda-test --use-cpu=all --no-half --no-half-vae'"
